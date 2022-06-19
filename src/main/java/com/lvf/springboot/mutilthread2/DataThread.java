e3a96ab506cd91fe29af4f5d973ce93eb0724648
children2018 <evj86@163.com>
2022-06-19 23:26:36 +0800

    6

---
+++ src/main/java/com/lvf/springboot/mutilthread2/DataThread.java
@@ -0,0 +1,40 @@
+package com.lvf.springboot.mutilthread2;
+
+import java.util.List;
+import java.util.UUID;
+
+public class DataThread<T> extends Thread {
+	
+	private Data<T> data = null;
+	private String uuid = UUID.randomUUID().toString();
+	private DataService<T> dataService = null;
+	
+	public DataThread(Data<T> data, DataService<T> dataService) {
+		this.data = data;
+		this.dataService = dataService;
+		System.out.println(System.currentTimeMillis() + " uuid:" + uuid + " created.");
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			List<T> list = data.getData();
+			
+			if (list == null || list.isEmpty()) {
+				break;
+			}
+			
+			System.out.println(System.currentTimeMillis() + " uuid:" + uuid + " list.size:" + list.size());
+//			try {
+//				Thread.sleep(100);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+			/*for (T t : list) {
+			}*/
+		}
+		System.out.println(System.currentTimeMillis() + " uuid:" + uuid + " done");
+		this.dataService.doneNum(uuid);
+	}
+
+}
---
+++ src/main/java/com/lvf/springboot/mutilthread3/Data.java
@@ -0,0 +1,36 @@
+package com.lvf.springboot.mutilthread3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.formula.functions.T;
+
+public class Data<T> {
+
+	private List<T> list;
+	private int size;
+
+	public Data(int size, List<T> list) {
+		this.list = list;
+		this.size = size;
+	}
+
+	public synchronized List<T> getData() {
+		
+		if (list == null || list.isEmpty()) {
+			return null;
+		}
+
+		List<T> result = new ArrayList<T>();
+		while (list.size() > 0) {
+			result.add(list.get(0));
+			list.remove(0);
+			if (result.size() >= size) {
+				break;
+			}
+		}
+		
+		return result;
+	}
+
+}
