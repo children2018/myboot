ef33036f053488976a268c586dd28b9eec8ff12a
children2018 <evj86@163.com>
2022-07-24 05:07:57 +0800

    主要是利用内存速度+多线程，还要考虑多线程之间的协同合作。

--- src/main/java/com/lvf/springboot/poi/cvs/CvsParser.java
+++ src/main/java/com/lvf/springboot/poi/cvs/CvsParser.java
@@ -7,20 +7,22 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.util.ResourceUtils;
 
 /**
  * 如果单元格内容存在敏感字符的情况下： 1、出现英文逗号，整个单元格内容会加上双引号，例：[我们,] -> ["我们,"]
  * 2、出现英文引号，整个单元格内容会加上双引号，例：[我们"爱]-> ["我们""爱"] 3、出现英文引号，每个引号都会调整为双引号，例：[我们"爱]->
  * ["我们""爱"]
  * "(\"[^\"]*(\"{2})*[^\"]*\")*[^,]*,"
- * @author administrator如果单元格内容存在敏感字符的情况下出现英文逗号整个单元
+ * @author Administrator
  *
  */
 public class CvsParser {
@@ -111,12 +113,12 @@ public class CvsParser {
 			sbf.append('\n');
 			in.write(sbf.toString());
 			for (int i = 1; i <= row; i++) {
-				sbf = new StringBuffer(); 
+				sbf = new StringBuffer();
 				for (int j = 1; j <= col; j++) {
 					int n = 10;
 					int m = 2;
 					int i1 = new Random().nextInt((n - m) + 1) + m;
-					for (int l =1 ; l <= i1 ;l ++) {
+					for (int l = 1; l <= i1; l++) {
 						n = content.length() - 1;
 						m = 0;
 						int i2 = new Random().nextInt((n - m) + 1) + m;
@@ -133,6 +135,229 @@ public class CvsParser {
 			}
 			in.flush();
 			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 1000万条记录, 单线程顺序查找特定字符用时2.8秒
+	 * 1000万条记录, 用LIST存储在内存当中，单线程顺序查找特定字符用时0.157秒
+	 */
+	@Test
+	public void readCsvFile() {
+		List<String> list = new ArrayList<String>();
+		try {
+
+			File f = new File("C:\\Users\\Administrator\\Desktop\\test.csv");
+			System.out.println("f.length():" + f.length());
+			InputStreamReader in = new InputStreamReader(new FileInputStream(f), "gbk");
+			BufferedReader read = new BufferedReader(in);
+			String line = new String();
+			int a = 0;
+			while ((line = read.readLine()) != null) {
+				list.add(line);
+			}
+			read.close();
+			in.close();
+			
+			new Thread(() -> {
+				long start = System.currentTimeMillis();
+				for (int i = list.size() / 4 * 3; i < list.size() ; i++) {
+					
+					if (!s) {
+						long end = System.currentTimeMillis();
+						System.out.println("cost4:" + (end - start));
+						return;
+					}
+					
+					if (list.get(i).indexOf("<[[-lfs-]]>") != -1) {
+						s = false;
+						long end = System.currentTimeMillis();
+						System.out.println("cost4:" + (end - start));
+						return;
+					}
+				}
+				long end = System.currentTimeMillis();
+				System.out.println("cost4:" + (end - start));
+			}).start();
+
+			new Thread(() -> {
+				long start = System.currentTimeMillis();
+				for (int i = list.size() / 4 * 0; i < list.size() / 4 * 1; i++) {
+					
+					if (!s) {
+						long end = System.currentTimeMillis();
+						System.out.println("cost1:" + (end - start));
+						return;
+					}
+					
+					if (list.get(i).indexOf("<[[-lfs-]]>") != -1) {
+						s = false;
+						long end = System.currentTimeMillis();
+						System.out.println("cost1:" + (end - start));
+						return;
+					}
+				}
+				long end = System.currentTimeMillis();
+				System.out.println("cost1:" + (end - start));
+			}).start();
+			
+			new Thread(() -> {
+				long start = System.currentTimeMillis();
+				for (int i = list.size() / 4 * 1; i < list.size() / 4 * 2; i++) {
+					
+					if (!s) {
+						long end = System.currentTimeMillis();
+						System.out.println("cost2:" + (end - start));
+						return;
+					}
+					
+					if (list.get(i).indexOf("<[[-lfs-]]>") != -1) {
+						s = false;
+						long end = System.currentTimeMillis();
+						System.out.println("cost2:" + (end - start));
+						return;
+					}
+				}
+				long end = System.currentTimeMillis();
+				System.out.println("cost2:" + (end - start));
+			}).start();
+
+			new Thread(() -> {
+				long start = System.currentTimeMillis();
+				for (int i = list.size() / 4 * 2; i < list.size() / 4 * 3; i++) {
+					
+					if (!s) {
+						long end = System.currentTimeMillis();
+						System.out.println("cost3:" + (end - start));
+						return;
+					}
+					
+					if (list.get(i).indexOf("<[[-lfs-]]>") != -1) {
+						s = false;
+						long end = System.currentTimeMillis();
+						System.out.println("cost3:" + (end - start));
+						return;
+					}
+				}
+				long end = System.currentTimeMillis();
+				System.out.println("cost3:" + (end - start));
+			}).start();
+			
+			Thread.sleep(1000 * 100);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 1000万条记录, 单线程顺序查找特定字符用时2.8秒
+	 */
+	static int count1 = 0;
+	static int count2 = 0;
+	boolean s = true;
+	@Test
+	public void readCsvFile2() {
+		try {
+			int strLength = 10000;
+			
+			CountDownLatch lll = new CountDownLatch(2);
+			long start = System.currentTimeMillis();
+			Thread kk = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						long start1 = System.currentTimeMillis();
+						File f = new File("C:\\Users\\Administrator\\Desktop\\test.csv");
+						System.out.println("f1.length():" + f.length());
+						InputStreamReader in;
+						in = new InputStreamReader(new FileInputStream(f), "gbk");
+						BufferedReader read = new BufferedReader(in);
+						char[] line = new char[strLength + 10];
+						read.skip(0);
+						while (read.read(line, 0, strLength) != -1) {
+							if (!s) {
+								lll.countDown();
+								long end1 = System.currentTimeMillis();
+								System.out.println("cost2-----------------::" + (end1 - start1));
+								return;
+							}
+							
+							String str = new String(line);
+							count1 ++;
+							//System.out.println("r1");
+							if (str.indexOf("<[[-lfs-]]>") != -1) {
+								lll.countDown();
+								s = false;
+								long end1 = System.currentTimeMillis();
+								System.out.println("cost1-----------------:" + (end1 - start1));
+								return;
+							}
+							line = new char[strLength + 10];
+						}
+						lll.countDown();
+						long end1 = System.currentTimeMillis();
+						System.out.println("cost1-----------------::" + (end1 - start1));
+					} catch (Exception e) {
+						e.printStackTrace();
+					} 
+				}
+			});
+			int end2 = 255000000;
+			Thread kk2 = new Thread(() -> {
+				try {
+					long start1 = System.currentTimeMillis();
+					File f = new File("C:\\Users\\Administrator\\Desktop\\test.csv");
+					System.out.println("f2.length():" + f.length());
+					InputStreamReader in;
+					in = new InputStreamReader(new FileInputStream(f), "gbk");
+					BufferedReader read = new BufferedReader(in);
+					char[] line = new char[strLength + 10];
+					read.skip(0);
+					read.skip(end2);
+					while (read.read(line, 0, strLength) != -1) {
+						
+						if (!s) {
+							lll.countDown();
+							long end1 = System.currentTimeMillis();
+							System.out.println("cost2-----------------::" + (end1 - start1));
+							return;
+						}
+						
+						String str = new String(line);
+						count2 ++;
+						//System.out.println("r2");
+						if (str.indexOf("<[[-lfs-]]>") != -1) {
+							lll.countDown();
+							s = false;
+							long end1 = System.currentTimeMillis();
+							System.out.println("cost2-----------------::" + (end1 - start1));
+							return;
+						}
+						line = new char[strLength + 10];
+					}
+					long end1 = System.currentTimeMillis();
+					System.out.println("cost2-----------------::" + (end1 - start1));
+					lll.countDown();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} 
+				
+			}
+			);
+			kk.start();
+			kk2.start();
+			lll.await();
+			
+			long end = System.currentTimeMillis();
+			System.out.println("count1:" + count1);
+			System.out.println("count2:" + count2);
+			System.out.println("cost end...:" + (end - start));
+			
+			Thread.sleep(1000 * 100);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
